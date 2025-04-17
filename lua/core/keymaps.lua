9a82c0d3e230052e4441d226571bb535b79d888f
Gabriel Sousa <gabriel-sousa@extreme.home>
2025-04-17 23:47:54 +0100

    Issue with harpoon keybinds must config on file

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -119,7 +119,7 @@ vim.keymap.set('n', '<leader>gl', vim.cmd.GitGutterEnable)
 vim.keymap.set('n', '<leader>gr', vim.cmd.Neogit)
 
 --Harpoon
-local mark = require("harpoon.mark")
+--[[local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set('n', '<leader>ha', mark.add_file)
@@ -129,6 +129,6 @@ vim.keymap.set('n', '<leader>h1', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<leader>h2', function() ui.nav_file(2) end)
 vim.keymap.set('n', '<leader>h3', function() ui.nav_file(3) end)
 vim.keymap.set('n', '<leader>h4', function() ui.nav_file(4) end)
-vim.keymap.set('n', '<leader>h5', function() ui.nav_file(5) end)
+vim.keymap.set('n', '<leader>h5', function() ui.nav_file(5) end)]]
 
 
