6ff78df2dbc299cf4b4a4cdc966d83f889f207e0
Gabriel Sousa <gabriel-sousa@extreme.home>
2025-04-17 23:41:32 +0100

    Installed harpoon and neogit. Made necessary keybinds for harpoon found under harpoon section. [No issues]

---
+++ lua/core/plugins/harpoon.lua
@@ -0,0 +1,8 @@
+return {
+    'ThePrimeAgen/harpoon',
+    lazy = false,
+    config = function()
+        local mark = require("harpoon.mark")
+        local ui = require("harpoon.ui")
+    end, 
+}
